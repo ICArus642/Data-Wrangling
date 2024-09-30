10ba3f749606a018d98224367f31882d49359581
Sebastián Hernández Martínez <142637555+Sebas-Hernandez05@users.noreply.github.com>
2024-09-30 18:46:20 -0500

    Update user_interface.py
    Se agregó función para mostrar gráfico y función de descripción de datos

--- data wrangling/ui/user_interface.py
+++ data wrangling/ui/user_interface.py
@@ -1,6 +1,17 @@
+import matplotlib.pyplot as plt
 from tabulate import tabulate
 
 #solo admite bibliotecas
 def impresion_datos_extraidos (results_df):
 
     print(tabulate(results_df, tablefmt="github", headers=["Número de registro", "Ciudad de ubicación","Departamento", "Edad", "Tipo", "Estado", "País de procedencia"]))
+    
+def impresion_estadistica (results_df):
+    print(results_df.describe())
+    
+def impresion_grafica (results_df):
+    results_df['edad'].value_counts().plot(kind='bar')
+    plt.xlabel('edad')
+    plt.ylabel('Número de casos')
+    plt.title('Número de casos por edad')
+    plt.show()
